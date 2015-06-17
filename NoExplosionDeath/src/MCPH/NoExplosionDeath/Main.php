7924ab6d270ba82e49d77bcd660aabf28f74cb31
Anton Magsino <MinecrafterPH@users.noreply.github.com>
2015-06-17 19:05:22 +0800

    Create Main.php

---
+++ NoExplosionDeath/src/MCPH/NoExplosionDeath/Main.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace MCPH\NoExplosionDeath
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+
+class Main extends PluginBase implements Listener
+{
+  
+   public function onEnable()
+   {
+     $this->getServer()->getPluginManager()->registerEvents($this, $this);
+     $this->getLogger()->info("NoExplosionDeath has been enabled.");
+   }
+   public function onDisable()
+   {
+     $this->getLogger()->info("NoExplosionDeath has been disabled.");
+   }
+   public function onDamage(EntityDamageEvent $event)
+   {
+     $cause = $event->getCause();
+     if($cause === EntityDamageEvent::CAUSE_BLOCK_EXPLOSION || $cause === EntityDamageEvent::CAUSE_ENTITY_EXPLOSION){
+     $event->setCancelled();
+     }
+   }
+}
+
+?>
+   
