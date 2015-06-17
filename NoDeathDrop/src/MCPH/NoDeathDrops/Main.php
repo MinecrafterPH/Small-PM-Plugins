5183c167eccaf8ef9f020fbb3e2149f957bba2e7
Anton Magsino <MinecrafterPH@users.noreply.github.com>
2015-06-17 18:03:05 +0800

    Create Main.php

---
+++ NoDeathDrop/src/MCPH/NoDeathDrops/Main.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MCPH\NoDeathDrops;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase implements Listener 
+{
+ public function onEnable()
+ {
+   $this->getServer()->getPluginManager()->registerEvents($this, $this);
+   $this->getLogger()->info("DeathNoDrops has been enabled!");
+ }
+ public function onDisable()
+ {
+   $this->getLogger()->info("DeathNoDrops has been disabled!");
+ } 
+ public function onDeath(PlayerDeathEvent $event)
+ {
+   $cfg = $this->getConfig();
+   $enable = $cfg->get("enable");
+   if($event->getEntity() instanceof Player and $enable !== true){
+       $event->setDrops([]);  
+     }
+   
+   
+ }
+}
