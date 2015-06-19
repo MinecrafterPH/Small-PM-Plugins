6d4ce461a6706b0b7dbd75cbf87fd268681dcce3
Anton Magsino <MinecrafterPH@users.noreply.github.com>
2015-06-19 17:08:05 +0800

    Update Main.php

--- NineIsBetterThanOne/src/MCPH/NIBTO/Main.php
+++ NineIsBetterThanOne/src/MCPH/NIBTO/Main.php
@@ -1,13 +1,23 @@
 <?php
 namespace MCPH\NIBTO;
+
+use pocketmine\Server;
 use pocketmine\item\Item;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase as Base;
+
 class Main extends Base implements Listener{
+    
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info("NIBTO has been enabled.")
     }
+    
+    public function onDisable(){
+        $this->getLogger()->info("NIBTO has been disabled.")
+    }
+    
     public function onBreak(BlockBreakEvent $event){
             if($event->getBlock()->getId() === 57){
                 $event->setDrops(array(Item::get(264, 0, 9)));
