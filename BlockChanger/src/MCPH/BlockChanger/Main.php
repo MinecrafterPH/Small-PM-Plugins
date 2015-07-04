018b9154f7cbad61be261c87af40a3659877a101
Anton Magsino <MinecrafterPH@users.noreply.github.com>
2015-07-04 12:02:24 +0800

    Update Main.php

--- BlockChanger/src/MCPH/BlockChanger/Main.php
+++ BlockChanger/src/MCPH/BlockChanger/Main.php
@@ -4,14 +4,18 @@ namespace MCPH\BlockChanger;
 
 use pocketmine\block\Block;
 use pocketmine\plugin\PluginBase;
+use pocketmine\level\Position;
+use pocketmine\utils\Config;
 
 class Main extends PluginBase{
   public function onEnable(){
     $cfg = $this->getConfig();
-    $x = $cfg->get("x");
-    $y = $cfg->get("y");
-    $z = $cfg->get("z")
-    $this->getServer()->getScheduler()->scheduleDelayedRepeatingTask(new Tasks\ChangeBlockTask($this, new Position($x, $y, $z, $this->getServer()->getDefaultLevel()), [Block::get(Block::WOOL, 0), Block::get(Block::WOOL, 15)]), 20, 20);
+    $ticks = $cfg->get("ticks");
+    $this->getServer()->getScheduler()->scheduleDelayedRepeatingTask(new Tasks\ChangeBlockTask($this, new Position($cfg->get("x1"), $cfg->get("y1"), $cfg->get("z1"), $this->getServer()->getDefaultLevel()), [Block::get(Block::WOOL, 0), Block::get(Block::WOOL, 15)]), $ticks, $ticks);
+    $this->getServer()->getScheduler()->scheduleDelayedRepeatingTask(new Tasks\ChangeBlockTask($this, new Position($cfg->get("x2"), $cfg->get("y2"), $cfg->get("z2"), $this->getServer()->getDefaultLevel()), [Block::get(Block::WOOL, 0), Block::get(Block::WOOL, 15)]), $ticks, $ticks);
+    $this->getServer()->getScheduler()->scheduleDelayedRepeatingTask(new Tasks\ChangeBlockTask($this, new Position($cfg->get("x3"), $cfg->get("y3"), $cfg->get("z3"), $this->getServer()->getDefaultLevel()), [Block::get(Block::WOOL, 0), Block::get(Block::WOOL, 15)]), $ticks, $ticks);
+    $this->getServer()->getScheduler()->scheduleDelayedRepeatingTask(new Tasks\ChangeBlockTask($this, new Position($cfg->get("x4"), $cfg->get("y4"), $cfg->get("z4"), $this->getServer()->getDefaultLevel()), [Block::get(Block::WOOL, 0), Block::get(Block::WOOL, 15)]), $ticks, $ticks);
+    $this->getServer()->getScheduler()->scheduleDelayedRepeatingTask(new Tasks\ChangeBlockTask($this, new Position($cfg->get("x5"), $cfg->get("y5"), $cfg->get("z5"), $this->getServer()->getDefaultLevel()), [Block::get(Block::WOOL, 0), Block::get(Block::WOOL, 15)]), $ticks, $ticks);
     $this->getLogger()->info("BlockChanger has been enabled.");
   }
   
