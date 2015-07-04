445ad8ee5e306090e6cf2a09eec733fab04e7fcb
Anton Magsino <MinecrafterPH@users.noreply.github.com>
2015-07-04 11:29:02 +0800

    Update Main.php

--- BlockChanger/src/MCPH/BlockChanger/Main.php
+++ BlockChanger/src/MCPH/BlockChanger/Main.php
@@ -7,7 +7,11 @@ use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase{
   public function onEnable(){
-    $this->getServer()->getScheduler()->scheduleDelayedRepeatingTask(new Tasks\ChangeBlockTask($this, new Position(125, 56, 63, $this->getServer()->getDefaultLevel()), [Block::get(Block::WOOL, 0), Block::get(Block::WOOL, 15)]), 20, 20);
+    $cfg = $this->getConfig();
+    $x = $cfg->get("x");
+    $y = $cfg->get("y");
+    $z = $cfg->get("z")
+    $this->getServer()->getScheduler()->scheduleDelayedRepeatingTask(new Tasks\ChangeBlockTask($this, new Position($x, $y, $z, $this->getServer()->getDefaultLevel()), [Block::get(Block::WOOL, 0), Block::get(Block::WOOL, 15)]), 20, 20);
     $this->getLogger()->info("BlockChanger has been enabled.");
   }
   
