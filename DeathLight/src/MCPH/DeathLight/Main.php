6f0b7f40305618481c4d5f7bdfa75a1711327e93
Anton Magsino <MinecrafterPH@users.noreply.github.com>
2015-11-14 16:42:16 +0800

    Create Main.php

---
+++ DeathLight/src/MCPH/DeathLight/Main.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace MCPH\DeathLight;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\network\protocol\AddEntityPacket;
+
+class Main extends PluginBase implements Listener
+{
+  
+  public function onEnable()
+  {
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    $this->getLogger()->info("DeathLight has been enabled.");
+  }
+  
+  public function onDisable()
+  {
+    $this->getLogger()->info("DeathLight has been disabled.");
+  }
+  
+  public function onMove(PlayerMoveEvent $e)
+  {
+    $p = $e->getPlayer();
+    $light = new AddEntityPacket();
+    $light->type = 93;
+    $light->eid = Entity::$entityCount++;
+    $light->metadata = array();
+    $light->speedX = 0;
+    $light->speedY = 0;
+    $light->speedZ = 0;
+    $light->x = $p->x;
+    $light->y = $p->y;
+    $light->z = $p->z;
+    $p->dataPacket($light);
+  }
+}  
+  
